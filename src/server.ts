d2c17d155d4f155300708f055b7b7d67969c54a4
srxrt <str9703@gmail.com>
2024-09-04 00:58:39 +0900

    Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("Executed");
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Martin";
+const count: number = 100;
