43e1319f1480e255ac99e7bb68b61b6e3e085ab8
srxrt <str9703@gmail.com>
2024-09-05 00:48:23 +0900

    Express integrations

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+export default app; // module.exports = app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+import app from "./app";
 
 import mongoose from "mongoose";
 
@@ -8,5 +9,8 @@ mongoose
 	.then((data) => {
 		console.log("MongoDB connection success");
 		const PORT = process.env.PORT || 3003;
+		app.listen(PORT, () => {
+			console.log(`Server is running successfully on port: ${PORT}`);
+		});
 	})
 	.catch((err) => console.log("ERROR on connection mongodb", err));
