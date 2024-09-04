5d965b0f3046e7031a6828114e2d689634242ba5
srxrt <str9703@gmail.com>
2024-09-04 10:06:58 +0900

    feat: Integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,2 @@
-console.log("Executed");
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "Martin";
-const count: number = 100;
+import dotenv from "dotenv";
+dotenv.config();
--- train.ts
+++ train.ts
@@ -1,1 +1,17 @@
 console.log("Train Area");
+
+function getHighestIndex(arr: number[]): number {
+	let hIndex: number = 0;
+
+	for (let i: number = 0; i < arr.length; i++) {
+		if (arr[i] > arr[i + 1] && arr[i] > arr[hIndex]) {
+			hIndex = i;
+		} else if (arr[i + 1] > arr[hIndex]) {
+			hIndex = i + 1;
+		}
+	}
+
+	return hIndex;
+}
+
+console.log(getHighestIndex([5, 21, 12, 21, 8]));
