3b533011edad0ed3884bf75632cbfc54db397679
srxrt <str9703@gmail.com>
2024-09-15 03:10:03 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,34 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDBSession(session);
+
+const store = new MongoDBStore({
+	uri: String(process.env.MONGO_URI),
+	collection: "sessions",
+});
+
 // entrance
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan(MORGAN_FORMAT));
+app.use(
+	session({
+		secret: String(process.env.SESSION_SECRET),
+		cookie: {
+			maxAge: 1000 * 3600 * 3, // 3h
+		},
+		store: store,
+		resave: true,
+		saveUninitialized: true,
+	})
+);
+
+// sessions
 
 // views
 app.set("views", path.join(__dirname, "views"));
