edd7aa67d031df56b5d6a4d790a71d527bedc9d9
srxrt <str9703@gmail.com>
2024-10-02 12:16:49 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -21,6 +21,27 @@ $(function () {
 		$(".dish-container").slideToggle(200);
 		$("#process-btn").css("display:flex");
 	});
+
+	$(".new-product-status").on("change", async (e) => {
+		const id = e.target.id;
+		const productStatus = $(`#${id}.new-product-status`).val();
+
+		try {
+			const response = await axios.post(`/admin/product/${id}`, {
+				productStatus: productStatus,
+			});
+			const result = response.data;
+			if (result.data) {
+				console.log("Product updated!");
+				$(".new-product-status").blur();
+			} else {
+				alert("Product update failed!");
+			}
+		} catch (err) {
+			console.log(err);
+			alert("Product update failed!");
+		}
+	});
 });
 
 function validateForm() {
