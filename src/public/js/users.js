85a1b831c6219f1d651418110a863de5056f4f3b
srxrt <str9703@gmail.com>
2024-10-02 12:43:49 +0900

    feat: frontend chosen user update

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,28 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+	$(".member-status").on("change", async (e) => {
+		const id = e.target.id;
+		const memberStatus = $(`#${id}.member-status`).val();
+		// nmaga classniyam iwlatdik, id orqali olsak bolardiku?
+
+		const response = await axios
+			.post(`/admin/user/edit`, {
+				_id: id,
+				memberStatus: memberStatus,
+			})
+			.then((response) => {
+				const result = response.data;
+				if (result.data) {
+					console.log("User updated!");
+					$(".member-status").blur();
+				} else {
+					alert("User update failed!");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				alert("User update failed!");
+			});
+	});
+});
