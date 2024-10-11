895a65a9ebdddb2d2e40a8cec90a4648a54f44f0
srxrt <str9703@gmail.com>
2024-10-11 17:32:00 +0900

    feat: T-TASK

--- train.ts
+++ train.ts
@@ -231,7 +231,7 @@ function calculate(str: string): number {
 }
 
 console.log(calculate("1+3"));
-*/
+
 // S-TASK:
 
 // Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va
@@ -247,3 +247,18 @@ function missingNumber(arr: number[]) {
 }
 
 console.log(missingNumber([3, 0, 1]));
+*/
+// T-TASK:
+
+// Shunday function yozing, u sonlardan tashkil topgan 2
+// ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+	arr1.forEach((ele) => {
+		arr2.push(ele);
+	});
+	return arr2.sort((a, b) => a - b);
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
