30d30d48cd6f72e1b3edc6e829002dddfada40ba
srxrt <str9703@gmail.com>
2024-10-19 20:48:19 +0900

    feat: develop getTopUsers business logic

--- src/router.ts
+++ src/router.ts
@@ -6,12 +6,21 @@ import uploader from "./libs/utils/uploader";
 /* Member*/
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
-router.get("/member/detail", memberController.verifyAuth, memberController.getMemberDetail);
+router.get(
+	"/member/detail",
+	memberController.verifyAuth,
+	memberController.getMemberDetail
+);
 router.post("/member/logout", memberController.verifyAuth, memberController.logout);
 router.post(
 	"/member/update",
 	memberController.verifyAuth,
 	uploader("members").single("memberImage"),
 	memberController.updateMember
+);
+router.get(
+	"/member/top-users",
+	memberController.verifyAuth,
+	memberController.getTopUsers
 );
 export default router;
